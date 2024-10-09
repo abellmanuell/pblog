b39e1f003b225d299b7b5b8425709009c20e909b
Abel Emmanuel <mannydev02@gmail.com>
2024-10-09 14:52:03 +0100

    [Fix]: Added className size-6 into the component

--- src/components/BlogIcon.jsx
+++ src/components/BlogIcon.jsx
@@ -1,12 +1,12 @@
 import { cn } from "../utils/cn";
 
-export default function IconLink({ icon: Icon, ...props }) {
+export default function BlogIcon({ icon: Icon, ...props }) {
   return (
     <div>
       <Icon
         className={cn(
           props.className,
-          "text-neutral-900 hover:text-neutral-600"
+          "text-neutral-900 hover:text-neutral-600 size-6"
         )}
       />
     </div>
